b1a0d58def18ac383af00df91f2eab6c520cae20
LiashenkoMA <makcim_an@mail.ru>
2024-02-25 20:19:32 +0400

    feat: вывод статей по категориям

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Categorie, CategorieSchema } from './categories.schema';
 import { CategorieController } from './categorie.controller';
 import { CategorieService } from './categorie.service';
+import { Pages, PagesSchema } from 'src/pages/pages.schema';
+import { PagesModule } from 'src/pages/pages.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Categorie.name, schema: CategorieSchema },
     ]),
+    MongooseModule.forFeature([{ name: Pages.name, schema: PagesSchema }]),
+    PagesModule,
   ],
   controllers: [CategorieController],
   providers: [CategorieService],
